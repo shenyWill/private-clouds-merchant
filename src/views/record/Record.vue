11f6fa780ca6005fb4ac70bd6bdcf24b717b59af
沈渊 <shenyuan@hfepay.com>
2018-11-20 16:08:59 +0800

    complete record except interface

--- src/views/record/Record.vue
+++ src/views/record/Record.vue
@@ -33,14 +33,39 @@
             <ul class="table-title-nav">
                 <li v-for="item in tableTitleList" :key="item" class="table-title-list">{{item}}</li>
             </ul>
-            <ul class="table-info-nav" v-for="(item, index) in tableList" :key="item.id">
-                <li class="table-info-list">{{index}}</li>
-                <li class="table-info-list">{{item.equipmentName}}</li>
-                <li class="table-info-list">{{item.personnelName}}</li>
-                <li class="table-info-list">{{item.personnelName}}</li>
-                <li class="table-info-list">{{item.personnelName}}</li>
-                <li class="table-info-list">{{item.personnelName}}</li>
+            <ul :class="['table-info-nav', childActiveList[index] ? 'active' : '']" v-for="(item, index) in tableList" :key="item.id">
+                <li class="table-info-list" @click="openChildTable(item, index)">{{index}}</li>
+                <li class="table-info-list" @click="openChildTable(item, index)">{{item.equipmentName}}</li>
+                <li class="table-info-list" @click="openChildTable(item, index)">{{item.personnelName}}</li>
+                <li class="table-info-list" @click="openChildTable(item, index)">{{item.personnelName}}</li>
+                <li class="table-info-list" @click="openChildTable(item, index)">{{item.personnelName}}</li>
+                <li class="table-info-list" @click="openChildTable(item, index)">{{item.personnelName}}</li>
+                <!-- 子表格 -->
+                <div class="child-table" v-if="childActiveList[index]">
+                    <ul class="child-table-title-nav">
+                        <li v-for="item in childTableTitleList" :key="item" class="child-table-title-list">{{item}}</li>
+                    </ul>
+                    <ul class="child-table-info-nav" v-for="childItem in childTableList" :key="childItem && childItem.id">
+                        <li class="child-table-info-list">{{childItem && childItem.recognitionTime && childItem.recognitionTime.split(' ')[1]}}</li>
+                        <li class="child-table-info-list">{{childItem && childItem.personnelName}}</li>
+                        <li class="child-table-info-list">{{childItem && childItem.personnelName}}</li>
+                        <li class="child-table-info-list">{{childItem && childItem.personnelName}}</li>
+                        <li class="child-table-info-list">{{childItem && childItem.personnelName}}</li>
+                    </ul>
+                </div>
             </ul>
+            <!-- 分页 -->
+            <el-pagination
+            background
+            layout="prev, pager, next"
+            v-if="count > 9"
+            :total="count"
+            :current-page.sync="currentPage"
+            class="paging"
+            @current-change="handleCurrentChange"
+            prev-text="上一页"
+            next-text="下一页"
+            ></el-pagination>
         </div>
     </div>
 </template>
@@ -66,9 +91,14 @@ export default {
       screenObj: {}, // 数据统计的筛选条件
       deviceList: [],
       tableTitleList: ['序号', '摄像头名称', '黑名单', '白名单', '访客', '其他'],
+      childTableTitleList: ['日期', '黑名单', '白名单', '访客', '其他'],
       timeActiveList: [true, false, false, false, false],
+      childActiveList: [], // 是否展开子表格
       tableList: [], // 列表中的统计数据
-      count: 0
+      childTableList: [], // 子表格中的统计数据
+      count: 0,
+      childCount: 0,
+      currentPage: 1
     };
   },
   computed: {
@@ -96,6 +126,35 @@ export default {
             this.count = response.data.data.total;
             this.tableList = response.data.data.rows;
         }
+    },
+    // 子表格展示
+    async childResponseAPI (data = {}) {
+        const response = await api.post(config.witness.list, data);
+        if (Number(response.data.code) === 200) {
+            this.childCount = response.data.data.total;
+            this.childTableList = response.data.data.rows;
+        }
+    },
+    // 展开子表格
+    async openChildTable (item, index) {
+        this.childActiveList.forEach((item, allindex) => (index !== allindex && (this.childActiveList[allindex] = false)));
+        this.$set(this.childActiveList, index, !this.childActiveList[index]);
+        if (this.childActiveList[index]) {
+            this.childResponseAPI();
+        } else {
+            this.childCount = 0;
+            this.childTableList = [];
+        }
+    },
+    // 关闭子表格
+    async closeChildTable () {
+        this.childActiveList = this.childTableList = [];
+        this.childCount = 0;
+    },
+    // 分页
+    handleCurrentChange (val) {
+        this.responseAPI({offset: val * 10});
+        this.closeChildTable();
     }
   },
   mounted () {
@@ -195,7 +254,7 @@ export default {
     ul,li {
         list-style: none;
     }
-    .table-title-nav,.table-info-nav {
+    .table-title-nav,.table-info-nav,.child-table-title-nav,.child-table-info-nav {
         height: 60px;
         width: 100%;
         overflow: hidden;
@@ -206,7 +265,7 @@ export default {
         font-size: 16px;
         font-weight: bold;
     }
-    .table-title-list,.table-info-list {
+    .table-title-list,.table-info-list,.child-table-title-list,.child-table-info-list {
         height: 60px;
         line-height: 60px;
         float: left;
@@ -221,12 +280,54 @@ export default {
             border-right: 0;
         }
     }
-    .table-info-nav {
+    .table-info-nav,.child-table-info-nav {
         background-color: #fff;
         border-top: 0;
         font-weight: normal;
+        transition: all 0.5s;
+        position: relative;
         &:nth-child(2n + 1) {
             background-color: #f8f8f8;
+        }
+        &.active {
+            height: 782px;
+            background: #cce8ff;
+            color: #008aff;
+            border: 1px solid #008aff;
+        }
+    }
+    .paging {
+        margin-top: 40px;
+        text-align: center;
+    }
+}
+// 子表格
+.child-table {
+    height: 720px;
+    min-width: 900px;
+    transition: all 2s;
+    width: 100%;
+    top: 60px;
+    left: 0;
+    position: absolute;
+    border-top: 1px solid #008aff;
+    background-color: #fff;
+    box-sizing: border-box;
+    padding: 30px;
+    .child-table-title-list {
+        width: 20%;
+        box-sizing: border-box;
+        color: #333;
+        &:first-child {
+            width: 20%;
+        }
+    }
+    .child-table-info-list {
+        border: 0;
+        width: 20%;
+        color: #666;
+        &:first-child {
+            width: 20%;
         }
     }
 }
@@ -246,5 +347,21 @@ export default {
     margin-right: 80px;
     margin-bottom: 15px;
   }
+}
+.record .statistics-table {
+    .paging .number,.paging .btn-next,.paging .btn-prev,.paging .more {
+        box-shadow: 3px 3px 2px 0 rgba(0, 0, 0, 0.2);
+        height: 40px;
+        border-radius: 4px;
+        background-color: #fff;
+    }
+    .paging .number,.paging .more {
+        width: 40px;
+        line-height: 40px;
+    }
+    .btn-next,.btn-prev {
+        width: 80px;
+        font-weight: bold;
+    }
 }
 </style>
